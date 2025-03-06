4ba45daaa98ed9a90985fb127f7062a10da5fdfd
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-03-06 22:34:27 +0300

    Interface update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cats_and_facts/requests/facts_requests.dart';
 import 'package:flutter/material.dart';
+import 'package:cats_and_facts/requests/image_requests.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,11 +24,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ImageRequests imageRequests = ImageRequests();
+  final FactsRequests factsRequests = FactsRequests();
+
+  String? _imageUrl;
+  String? _facts = 'After clicking on the button you will receive a fact and a picture of the cat';
+  bool loading = false;
+
+  // Метод для загрузки картинки
+  Future<void> resourceExtraction() async {
+    setState(() {
+      loading = true;
+    });
+
+    final imageUrl = await imageRequests.getImage();
+    final facts = await factsRequests.getFact();
+
+    setState(() {
+      _imageUrl = imageUrl;
+      _facts = facts;
+      loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String? imageUrl;
-    String facts =
-        'Click on the button and get a fact about the cat and a picture';
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -38,28 +60,77 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
       ),
       body: Center(
-        child: Padding(
-        padding:EdgeInsets.only( top: 400),
-         child: Text(
-          facts,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontFamily: 'Playwrite', fontSize: 20, color: Colors.black, height: 1.3,
-           ),
-        ), 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 350,
+              height: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 44, 47, 81),
+                  width: 5,
+                ),
+              ),
+
+              child:
+                  loading
+                      ? Transform.scale(
+                        scale: 0.5, 
+                        child: CircularProgressIndicator(color: Color.fromARGB(255, 44, 47, 81)),
+                      )
+                      : _imageUrl == null
+                      ? const Text(
+                        ' ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 1,
+                          fontFamily: 'PlayWrite',
+                        ),
+                      )
+                      : Image.network(
+                        _imageUrl!,
+                        height: 350,
+                        width: 350,
+                        alignment: Alignment.center,
+                        fit: BoxFit.cover,
+                      ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.only(left: 30, right: 30),
+              child: Text(
+                _facts! ?? "No facts available",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'PlayWrite',
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: 50, left: 100,right: 100),
+        padding: EdgeInsets.only(bottom: 40, left: 100, right: 100),
         child: SizedBox(
           width: 200,
           height: 50,
           child: OutlinedButton(
             onPressed: () {
+              resourceExtraction();
               print('Нажата OutlinedButton');
             },
             style: OutlinedButton.styleFrom(
               backgroundColor: Color.fromARGB(255, 243, 170, 194),
-              side: const BorderSide(color: Colors.black, width: 1),
+              side: const BorderSide(
+                color: Color.fromARGB(255, 44, 47, 81),
+                width: 3,
+              ),
             ),
             child: const Text(
               'Get a cat',
